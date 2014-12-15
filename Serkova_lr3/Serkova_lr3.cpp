6231adf6ce82f328b159091a3dee60e444271c1d
sova93 <s_o_va@tut.by>
2014-12-15 21:22:21 +0200

    l3_Update1

---
+++ Serkova_lr3/Serkova_lr3.cpp
@@ -0,0 +1,13 @@
+// Serkova_lr3.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include "windows.h"
+#include <iostream>
+#include "math.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
